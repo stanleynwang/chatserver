72c8c0b26269dd81103b41414a62541a2dc8b260
Stanley Wang <stanleynwang@gmail.com>
2012-05-14 00:17:01 -0700

    Server now listens on port env var

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
     res.writeHead(200, {'Content-Type':'text/html'});
     res.end();
 });
-server.listen(8080);
+if (process.env.PORT !== "")
+    process.env.PORT = 8080;
+server.listen(process.env.PORT);
 
 var nowjs = require("now");
 var everyone = nowjs.initialize(server);
